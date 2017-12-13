5f1cafcba4097a523742d85efce07457da11730e
Alexandre Beelen <alexandre.beelen@lam.fr>
2017-12-13 15:14:46 +0100

    Add logo

--- nikamap/__init__.py
+++ nikamap/__init__.py
@@ -1,3 +1,15 @@
+r"""
+ _   _ _ _        ___  ___
+| \ | (_) |       |  \/  |
+|  \| |_| | ____ _| .  . | __ _ _ __
+| . ` | | |/ / _` | |\/| |/ _` | '_ \
+| |\  | |   < (_| | |  | | (_| | |_) |
+\_| \_/_|_|\_\__,_\_|  |_/\__,_| .__/
+                               | |
+                               |_|
+======================================
+"""
+
 from __future__ import absolute_import, division, print_function
 from .version import __version__  # noqa
 from .nikamap import *  # noqa
